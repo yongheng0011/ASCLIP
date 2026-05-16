e8c93d1fdbc8042ffd25684abffa5391a7ef5b13
XX1 <96614058+yongheng0011@users.noreply.github.com>
2026-05-16 15:54:30 +0800

    Update train_asclip.sh

--- train_asclip.sh
+++ train_asclip.sh
@@ -6,7 +6,7 @@ CUDA_VISIBLE_DEVICES=0 python train_fenkuai_up.py --dataset mvtec --train_data_p
 --save_path /workspace/project/AdaptCLIP/train_fenkuai_sim/use_mvtec \
 --k_shots 0 \
 --features_list 6 12 18 24 --image_size 518  --batch_size 8  --print_freq 1 \
---epoch 15 --save_freq 1  \
+--epoch 10 --save_freq 1  \
 --n_ctx 12  --vl_reduction 4 \
 --visual_learner --textual_learner  \
 --up sim
